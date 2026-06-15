d9da5541985929c7dc47ef3213ca1dbb563be153
johanna <167251945+johannabruns@users.noreply.github.com>
2026-06-15 16:53:54 +0200

    projekt aufgeräumt

--- Assets/Scripts/Interactibles/SimpleInteractible.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-/// <summary>
-/// An interactible that only responds to a single button press, such as a lever or a button.
-/// </summary>
-public abstract class SimpleInteractible : Interactible
-{
-    public abstract void OnInteract();
-}
--- Assets/Scripts/Weather Effects/ILightningTarget.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public interface ILightningTarget
-{
-    abstract void OnStruck();
-}
